a9e68a729b54b4bd90d3db4af2a013c064b6ed4b
Sergio <alonsodecaso.sergio@gmail.com>
2021-04-27 17:24:23 +0200

    Combos de fechas listos junto a las funciones de cargarNumeroDias y calcularDiasMes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function cargarDatos(){
+    cargarAnio();
+    cargarNumeroDias();
+}
+
 function cargarAnio(){
 
     var select = document.getElementsByName("anio");
@@ -8,4 +13,64 @@ function cargarAnio(){
         option.innerHTML = i;
         select[0].appendChild(option);
     }    
+}
+
+function cargarNumeroDias(){
+
+    var comboDia=document.getElementsByName("dia");
+
+    var comboAnio=document.getElementsByName("anio")[0];
+    var comboMes=document.getElementsByName("mes")[0];
+
+    var numeroDias=calcularDiasMes(comboAnio.options[comboAnio.selectedIndex].value, comboMes.selectedIndex+1); 
+
+    //VACIA EL COMBO PARA VOLVERLO A RELLENAR CON LOS DIAS
+    var length = comboDia[0].options.length;
+    for (i = length-1; i >= 0; i--) {
+    comboDia[0].options[i] = null;
+    }
+
+    for (var i= 1; i <= numeroDias; i++) {
+
+        var option = document.createElement("option");
+        option.innerHTML = i;
+        comboDia[0].appendChild(option);
+
+    }
+
+}
+
+function calcularDiasMes(anio, mes) {
+
+    var numeroDias = -1;
+
+    switch (mes) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            numeroDias = 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            numeroDias = 30;
+            break;
+        case 2:
+
+
+            if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 100 == 0 && anio % 400 == 0)) { //Con esto sé si es bisiesto o no
+                numeroDias = 29;
+            } else {
+                numeroDias = 28;
+            }
+            break;
+
+    }
+
+    return numeroDias;
 }
